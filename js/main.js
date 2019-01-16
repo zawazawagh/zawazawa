54b05e083389a362ffaff2e7842b5ca146a92634
Kengo Ozawa <kengorou@ozwspc.local>
2019-01-16 20:03:01 +0900

    wip

--- js/main.js
+++ js/main.js
@@ -11,24 +11,54 @@ jQuery(function($) {
 //モーダルウィンドウ
 $(function () {
 	var id = "#openModal";
+	var modalid = "#modalArea";
 	var count = 0;
-	do {
-	  	$(String(id + count)).click(function(){
-	  	    $('#modalArea').fadeIn();
-	  	});
-	  	 $(String(id + count)).click(function(){
-	  	    $('#modalArea').fadeIn();
-	  	});
-	count++;
-	} while (count < 10);
-  	$('#closeModal , #modalBg').click(function(){
-  	$('#modalArea').fadeOut();
+
+	// do {
+	//   	$(String(id + count)).click(function(){
+	//   	    $(String(modalid + count)).fadeIn();
+	//   	});
+	// 	count++;
+	// 	console.log(modalid + count);
+	// } while (count < 8);
+
+	$('#openModal1').click(function(){
+	    $('#modalArea1').fadeIn();
   	});
+	$('#openModal2').click(function(){
+	    $('#modalArea2').fadeIn();
+  	});
+	$('#openModal3').click(function(){
+	    $('#modalArea3').fadeIn();
+  	});
+	$('#openModal4').click(function(){
+	    $('#modalArea4').fadeIn();
+  	});
+	$('#openModal5').click(function(){
+	    $('#modalArea5').fadeIn();
+  	});
+	$('#openModal6').click(function(){
+	    $('#modalArea6').fadeIn();
+  	});
+	$('#openModal7').click(function(){
+	    $('#modalArea7').fadeIn();
+  	});
+	$('#openModal8').click(function(){
+	    $('#modalArea8').fadeIn();
+  	});
+  	// $('#closeModal , #modalBg').click(function(){
+  	// $('#modalArea').fadeOut();
+  	// });
 });
 
 
 // 各ページの共通部分を外部化
 $(function(){
 	$("#header").load("layouts/header.html");
+	$("#modals").load("layouts/modals.html", function() {
+		$('#closeModal , #modalBg').click(function(){
+  		$('.modalArea').fadeOut();
+  		});
+	});
 });
 
