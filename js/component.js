5497e8d2f82573a606aa4775c29238123361611f
zawazawagh <kengorou2014@gmail.com>
2020-04-26 12:36:50 +0900

    tmp

--- js/component.js
+++ js/component.js
@@ -7,3 +7,25 @@ AFRAME.registerComponent('color-randomizer', {
       });
     }
 });
+
+AFRAME.registerComponent('registerevents', {
+  init: function () {
+    var marker = this.el;
+    // Make the element emit events when found and when lost.
+    marker.setAttribute('emitevents', 'true');
+    marker.addEventListener('markerFound', function() {
+      var markerId = marker.id;
+      var els = document.querySelectorAll('.lazystart');
+      els.forEach( function( el, _) {
+        el.setAttribute("animation", {enabled : true});
+        el.setAttribute("animation__2", {enabled : true});
+      })
+      var videoEl = document.querySelector('.my-video');
+      videoEl.play();
+    });
+    marker.addEventListener('markerLost', function() {
+      var markerId = marker.id;
+      console.log('markerLost', markerId);
+    });
+  }
+});
