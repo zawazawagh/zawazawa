a48ea222e7c8fb5d69c4841facf0349be26a245d
zawazawagh <kengorou2014@gmail.com>
2020-05-03 22:15:45 +0900

    fix play video

--- js/component.js
+++ js/component.js
@@ -50,22 +50,3 @@ AFRAME.registerComponent('registerevents', {
     });
   }
 });
-
-AFRAME.registerComponent('vidhandler', {
-  init: function() {
-      this.toggle = false;
-      this.vid = document.querySelector("#vid");
-      this.vid.pause();
-  },
-  tick: function() {
-      if (this.el.object3D.visible == true) {
-          if (!this.toggle) {
-              this.toggle = true;
-              this.vid.play();
-          }
-      } else {
-          this.toggle = false;
-          this.vid.pause();
-      }
-  }
-});
